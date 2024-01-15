61d73b6e288ff661ab1071ccfc19fb0d169be423
Mohd Kaif <mohdkaif97169@gmail.com>
2024-01-15 17:37:50 +0530

    19_scope2.js

---
+++ 03_basics/19_scope2.js
@@ -0,0 +1,21 @@
+// one method to create functions is
+// console.log(one(5));
+// in this declaration of function access before definition is not gives error
+// function one(num) {
+//     return num + 1
+// }
+
+
+// one another method to create functions is
+// in this declaration of function access before definition is gives error
+console.log(two(5));
+const two = function(num) {
+    return num + 2
+}
+
+
+
+
+
+
+
