7b96d65e835c9abdff45eabd394b2c02ad95c5e8
Mohd Kaif <mohdkaif97169@gmail.com>
2024-01-15 17:02:06 +0530

    18_scope.js

---
+++ 03_basics/18_scope.js
@@ -0,0 +1,16 @@
+var c = 7
+
+let a = 8
+
+const b = 100
+{
+    let a = 4
+    const b = 5
+    var c = 700
+
+}
+
+console.log(a);
+console.log(b);
+
+console.log(c); // this will create the problem in the code so that it can not be used in js that's why let and const is created to solve this problem !
