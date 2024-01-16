559c26038865d9078d61e4c37793cb058cc0ed3f
Mohd Kaif <mohdkaif97169@gmail.com>
2024-01-16 22:01:10 +0530

    22_ifElse.js

---
+++ 04_control_Flow/22_ifElse.js
@@ -0,0 +1,9 @@
+
+// other things are same as in another language
+const age = 20
+
+if(age < 18) {
+    console.log(`your age is less than 18 you can not vote`);
+} else {
+    console.log(`your age is greater than 18 you can vote`);
+}
