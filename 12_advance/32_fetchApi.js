b41ac049d40b011bf1dca2543b0aa971808fa155
Mohd Kaif <mohdkaif97169@gmail.com>
2024-01-30 21:09:33 +0530

    fetchApi

--- 12_advance/32_fetchApi.js
+++ 12_advance/32_fetchApi.js
@@ -1,8 +1,46 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
 
-const asynFunction = async () => {
-    const respose = await fetch(URL);
-    console.log(respose);
-    const data = await respose.json();
-    console.log(data);
-};
+// let imageURL = document.querySelector('.userImg');
+// let followersURL = document.querySelector('.followers');
+// let btn =  document.querySelector('.btn');
+// async function linkOfUserInfo() {
+//     const mainURL = "https://api.github.com/users/MohdKaif1729"
+//     const promise = await fetch(mainURL);
+//     const response = await promise.json();
+
+//     imageURL = response.avatar_url;
+//     followersURL = response.followers_url;
+
+//    btn.addEventListener('click', () => {
+//         imageURL.style.src = imageURL;
+//         followersURL.style.innnerHTML = followersURL;
+        
+//     })
+    
+//     console.log(imageURL);
+//     console.log(followersURL);
+// }
+
+// linkOfUserInfo();
+
+let imageURL = document.querySelector('.userImg');
+let followersURL = document.querySelector('.followers');
+let btn = document.querySelector('.btn');
+
+async function linkOfUserInfo() {
+  const mainURL = "https://api.github.com/users/MohdKaif1729"
+  const promise = await fetch(mainURL);
+  const response = await promise.json();
+
+  
+  btn.addEventListener('click', () => {
+      imageURL.src = response.avatar_url;
+      followersURL.innerHTML = `Followers: ` + response.followers;
+    // Do something on button click if needed
+  });
+
+  console.log(imageURL);
+  console.log(followersURL);
+}
+
+linkOfUserInfo();
+
