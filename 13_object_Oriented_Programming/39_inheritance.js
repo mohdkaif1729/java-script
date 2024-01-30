bfaa8523d2f98ed97885c0b4435e68dd5b071c39
Mohd Kaif <mohdkaif97169@gmail.com>
2024-01-30 21:11:42 +0530

    inheritance

---
+++ 13_object_Oriented_Programming/39_inheritance.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username) {
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password) {
+        // here this is same as call
+        super(username)
+        this.email = email
+        this.password = password
+    } 
+
+    addCourse() {
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.addCourse();
+
+const masalaChai = new User("masalaChai")
+masalaChai.logMe()
+
+// console.log(chai === masalaChai);
+console.log(masalaChai instanceof User);
