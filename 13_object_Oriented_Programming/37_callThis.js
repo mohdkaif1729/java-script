2606133f93e1995067ac9129daa094582dd57d3a
Mohd Kaif <mohdkaif97169@gmail.com>
2024-01-30 21:11:15 +0530

    call and this keyword

---
+++ 13_object_Oriented_Programming/37_callThis.js
@@ -0,0 +1,15 @@
+function setUserName(username) {
+    // complex DB calls
+    this.username = username;
+} 
+
+function createUser(username, email, password) {
+    // setUserName(this, username)
+    setUserName.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+
+console.log(chai);
