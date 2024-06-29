30ba77279fe5f0663c92562efa004711b2cc4c24
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-06-29 12:09:49 +0000

    math pi property is change

--- 13_object_Oriented_Programming/42_mathPI.js
+++ 13_object_Oriented_Programming/42_mathPI.js
@@ -3,4 +3,32 @@
 
 
 // let d = k.ceil()
-console.log(Math.ceil(Math.PI));
+// console.log(Math.ceil(Math.PI));
+
+// You can not change the property of any object in js
+// const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
+
+// console.log(descripter);
+
+
+const chai = {
+  name: "ginger chai",
+  price: 250,
+  isAvailable: true,
+}
+
+// console.log(chai);
+
+let descripter = Object.getOwnPropertyDescriptor(chai, "name");
+
+
+console.log(descripter);
+
+Object.defineProperty(chai, "name", {
+  writable: false,
+  enumerable: false,
+})
+
+descripter = Object.getOwnPropertyDescriptor(chai, "name");
+
+console.log(descripter);
